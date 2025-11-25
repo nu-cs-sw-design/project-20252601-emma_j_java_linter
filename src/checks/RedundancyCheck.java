5630af771ab98d21353a6b5723147adfc0b5f9fd
emmaj6001 <emmajohnston600@gmail.com>
2025-11-24 20:17:34 -0600

    redundancy check

--- src/checks/RedundancyCheck.java
+++ src/checks/RedundancyCheck.java
@@ -1,5 +1,24 @@
 package checks;
+import asm.ClassInfo;
+
+//import java.util.List;
+import java.util.Set;
 
 public class RedundancyCheck {
+
+    public void run(ClassInfo info) {
+        System.out.println("running redundancy check...");
+        String className = info.className;
+        Set<String> dependencies = info.referencedClasses;
+
+        if ((className == null) || (dependencies == null))
+            return; //nothing to check
+
+        //check for self-dependency and self-inheritance
+        if (dependencies.contains(className)) {
+            System.out.println("RedundancyCheck Warning: Class '" + className
+            + "' references itself, may indicate redundant code.");
+        }
+    }
     
 }
